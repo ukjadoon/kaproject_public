707331cc7d598183ac0e51b49ce5f1a4e8d3555c
Umar Jadoon <umar@cinch.nu>
2020-07-11 11:33:17 +0300

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container mx-auto py-12 sm:px-6 lg:px-8">
+        <h2 class="text-2xl font-bold">KA - Demo</h2>
+
+        <div class="cui-embed" style="height: 400px; width: 100%;" data-cui-uid="yhAclPpm" data-cui-avatar="https://images.typeform.com/images/Xq64aPwrZHpi" data-cui-mode="widget" data-cui-pill-button-color="#0000FF"></div><script src="https://public-assets.typeform.com/confab/embed.js" async></script>
+    </div>
+@endsection
