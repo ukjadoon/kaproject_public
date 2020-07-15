96913f97a165f4cad1dcb965fcaa9aa3c22d9f31
Umar Jadoon <umar@cinch.nu>
2020-07-16 00:13:37 +0300

    added the TallStack and Pest

--- public/js/app.js
+++ public/js/app.js
@@ -138,8 +138,8 @@ eval("// removed by extract-text-webpack-plugin//# sourceURL=[module]\n//# sourc
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! G:\laragon\www\kaproject\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! G:\laragon\www\kaproject\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/ukjadoon/kaproject/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/ukjadoon/kaproject/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
