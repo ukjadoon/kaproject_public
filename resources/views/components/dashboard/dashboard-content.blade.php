b47de03bb7d34ded24cdc55f0d195c93a554789d
Umar Jadoon <umar@cinch.nu>
2020-07-21 19:50:09 +0300

    added a loading state

--- resources/views/components/dashboard/dashboard-content.blade.php
+++ resources/views/components/dashboard/dashboard-content.blade.php
@@ -5,6 +5,9 @@
         </div>
         <div class="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
             <!-- Replace with your content -->
+            <div class="py-4" wire:loading>
+                Loading...
+            </div>
             <div class="py-4">
                 <div>{{ $slot }}</div>
             </div>
